34bb5e7d548d9c506e6dee1f74a2b29b195f5d3d
samandera <ela.lach@gmail.com>
2021-01-14 12:57:38 +0100

    Set subscriptions

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer, gql, PubSub } from 'apollo-server'
 import { importSchema } from 'graphql-import'
 import { applyMiddleware } from 'graphql-middleware'
 import { makeExecutableSchema } from 'graphql-tools'
+
+const pubSub = new PubSub()
 
 const server = new ApolloServer({
   schema: applyMiddleware(
     makeExecutableSchema({
       resolvers: {
+        Subscription: {
+          messageSent: {
+            subscribe: () => pubSub.asyncIterator(['CHAT_CHANNEL'])
+          }
+        },
         Query: {
           chat(_, args, ctx, info) {
             return 'chat'
+          }
+        },
+        Mutation: {
+          sendMessage(_, { message }, ctx, info) {
+            pubSub.publish('CHAT_CHANNEL', { messageSent: message })
+            return message;
           }
         }
       },
